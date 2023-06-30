9bee714d8e51baeadbdc75c7722507a61a264cb2
Navin Narayanan <nan.navinnarayanan@gmail.com>
2023-06-30 10:21:58 -0500

    Co-authored-by: CoolKingGreat <CoolKingGreat@users.noreply.github.com>

--- src/app/(home)/api/validate-user/route.ts
+++ src/app/(home)/api/validate-user/route.ts
@@ -1,14 +1,22 @@
+import { promises as fs } from 'fs'
+import path from 'path'
+
 export async function POST(req: Request) {
-    
+  const filePath = path.join(process.cwd(), 'project_files', 'passcodes.txt')
+  const fileContents = await fs.readFile(filePath, 'utf8')
+  console.log(fileContents)
+  let b = fileContents.trim().replace(/\n/g, ',').split(',')
 
+  // const jsonObject = {};
+  // for (let i = 0; i < b.length; i += 2) {
+  //   const key = b[i].trim();
+  //   const value = b[i + 1].trim();
+  //   jsonObject[key] = value;
+  // }
 
-    
-    
-    const data = await req.json()
-    if (data.userchoice == 'admin') {
-      return new Response(JSON.stringify({ choice: 'admin' }), { status: 200 })
-    } else if (data.userchoice == 'reserve') {
-        return new Response(JSON.stringify({ choice: 'reserve' }), { status: 200 })
-    }
-    return new Response(JSON.stringify({ status: 'ok' }), { status: 200 })
-  }
+// const jsonString = JSON.stringify(jsonObject);
+
+  console.log(b)
+  const data = await req.json()
+  return new Response(JSON.stringify({ status: 'ok' }), { status: 200 })
+}
